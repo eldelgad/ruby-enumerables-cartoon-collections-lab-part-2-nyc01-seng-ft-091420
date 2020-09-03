20aea3c56d33b00fee2c1b5e183f5c4eb9db0139
Elias Delgado <evdelgado99@gmail.com>
2020-09-03 06:28:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,12 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   
   planeteer_calls.map do |element| element.length > 4
-    return true
+    
+    if element.length > 4
+      return true
+    else
+      return false
+    end
   end
   
 end
@@ -32,4 +37,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
+  
+  
+  
 end
